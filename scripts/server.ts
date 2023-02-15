c56e9621a2c93e5b9576007a7601c88c67b13542
Patrick Canfield <pscale01@gmail.com>
2023-02-15 15:14:05 -0800

    paths are weird

--- scripts/server.ts
+++ scripts/server.ts
@@ -1,6 +1,6 @@
 #!/usr/bin/env -S deno run --allow-read --allow-net --allow-env --allow-write --allow-run
 
-import { render } from "~/render.ts";
+import { render } from "../src/render.ts";
 import { start } from '~/server/mod.ts'
 
 render()
