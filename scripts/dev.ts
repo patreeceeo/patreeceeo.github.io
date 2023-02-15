46896d3e313bef8cae268a425af037fb9706b4aa
Patrick Canfield <pscale01@gmail.com>
2023-02-15 15:37:41 -0800

    ugly fix for u, Deno Deploy

--- scripts/dev.ts
+++ scripts/dev.ts
@@ -1,10 +1,13 @@
 #!/usr/bin/env -S deno run --allow-write --allow-read --allow-run --allow-env
 
-const process = Deno.run({
-  cmd: ["deno", "run", "--allow-read", "--allow-net", "--allow-env", "--allow-write", "--allow-run", "./scripts/server.ts", "/docs"],
+import { render } from "~/render.ts";
+
+await render()
+
+const server = Deno.run({
+  cmd: ["deno", "run", "--allow-read", "--allow-net", "--allow-env", "--allow-write","--allow-run", "./scripts/server.ts", "/docs"],
   env: {
     ENV: "dev"
-  },
-});
-await process.status()
-
+  }
+})
+await server.status()
--- scripts/server.ts
+++ scripts/server.ts
@@ -1,7 +1,86 @@
 #!/usr/bin/env -S deno run --allow-read --allow-net --allow-env --allow-write --allow-run
 
-import { render } from "../src/render.ts";
-import { start } from '~/server/mod.ts'
+import { serve } from "http";
+import { extname } from "path";
+import { contentType as getContentType } from "media_types";
 
-render()
-start()
+const ROOT = `${Deno.cwd()}${Deno.args[0] || ""}`;
+const STATIC_PATH = "/static";
+let indexHtml = await Deno.readFile(`${ROOT}/index.html`);
+
+const notFoundHtml = "404: Not found";
+
+let devSocket: WebSocket
+if (Deno.env.get("ENV") === "dev") {
+
+  const { addProjectSourceModifyListener } = await import("~/util.ts")
+
+  addProjectSourceModifyListener(async () => {
+    const { render } = await import("~/render.ts");
+    await render()
+
+    indexHtml = await Deno.readFile(`${ROOT}/index.html`);
+
+    if(devSocket?.readyState === devSocket?.OPEN) {
+      devSocket.send(JSON.stringify({type: "resourceUpdate"}))
+    }
+  })
+}
+
+await serve(async (request) => {
+  const url = new URL(request.url);
+  const assetPath = `${ROOT}${url.pathname}`;
+  const assetContentType = getContentType(extname(url.pathname));
+  const htmlPath = `${ROOT}${url.pathname}/index.html`;
+  if (url.pathname === "/") {
+    return htmlResponse(indexHtml);
+  }
+
+  // TODO isDev fn
+  if (Deno.env.get("ENV") === "dev" && url.pathname === "/devSocket") {
+    console.log("devSocket requested!")
+    const {socket, response} = Deno.upgradeWebSocket(request)
+    socket.onopen = () => {
+      console.log("devSocket open!")
+    }
+    devSocket = socket
+    return response
+  }
+
+  if (
+    url.pathname.startsWith(STATIC_PATH) &&
+    (await isFile(assetPath)) &&
+  assetContentType
+  ) {
+    const content = await Deno.readFile(assetPath);
+    return new Response(content, {
+      headers: {
+        "content-type": assetContentType,
+      },
+    });
+  }
+
+  if (await isFile(htmlPath)) {
+    const html = await Deno.readFile(htmlPath);
+    return htmlResponse(html);
+  }
+
+  return htmlResponse(notFoundHtml);
+});
+
+async function isFile(path: string) {
+  try {
+    const info = await Deno.lstat(path);
+    return info.isFile;
+  } catch {
+    return false;
+  }
+}
+
+function htmlResponse(html: BodyInit) {
+  return new Response(html, {
+    headers: {
+      "content-type": "text/html",
+    },
+  });
+}
