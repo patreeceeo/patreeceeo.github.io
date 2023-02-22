932c5369a1d042daa10b61abe2b54ea2d7f393e8
Patrick Canfield <pscale01@gmail.com>
2023-02-22 10:56:48 -0800

    reword

--- src/pages/Home/mod.ts
+++ src/pages/Home/mod.ts
@@ -8,7 +8,7 @@ const kindWords: Array<KindWordsProps> = [
   {
     photo: "images/Aidan.webp",
     name: "Aidan Caruso",
-    distinction: "Software Engineer @ XREngine, formerly at Mappa Labs",
+    distinction: "Engineer & Technical Artist, Mappa Labs, 2023",
     quote: `
         <p>
         Patrick got me on my feet with some new technologies: Code reviews from him were informative, being detail-oriented but without missing the bigger picture, which really optimized what I had wrote. In-line with code reviews he helped set up an efficient Github workflow that streamlined our development process. All around, he was a substantial help to both myself and our project.
@@ -19,7 +19,7 @@ const kindWords: Array<KindWordsProps> = [
   {
     photo: "images/MikeDavis.jpeg",
     name: "Mike Davis",
-    distinction: "Product @ Amazon, formerly Sr SWE @ Constant Contact",
+    distinction: "Senior Engineer, Constant Contact, 2014",
     quote: `
         <p>
         Patrick is both an incredibly skilled and intelligent engineer (not to mention person in general). He strives for high code quality and ensures the bar is held high for his teammates too. His experience and insight makes him a great addition to any team. I would happily work with Patrick again.
