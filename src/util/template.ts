4922d4acb8dfc90d3cff7f53200c19e44c258f06
Patrick Canfield <pscale01@gmail.com>
2023-02-21 19:12:16 -0800

    support branches in templates

--- src/util/template.ts
+++ src/util/template.ts
@@ -1,7 +1,9 @@
-import { LinkStruct, _getStyleSheets } from "../internal/build_state.ts"
-import { readTextFileFromModule } from "../util.ts"
+import { LinkStruct, _getStyleSheets } from "~/internal/build_state.ts"
+import { readTextFileFromModule } from "~/util.ts"
 
 const templateSlotRE = /\$\{(.*?)\}/g
+const templateBranchStartRE = /^\s*<!-- IF (.*?) -->$/gm
+const templateBranchEndRE = /^\s*<!-- FI (.*?) -->$/gm
 
 export type TemplateValue = string | number
 export type AsyncTemplateData = Record<string, Promise<TemplateValue> | TemplateValue>
@@ -18,7 +20,7 @@ export async function renderTemplate(template: string, data: AsyncTemplateData) 
     resolvedData[key] = await data[key]
   }
   Object.assign(resolvedData, derivedData)
-  return renderTemplateBasic(template, resolvedData)
+  return evaluateSlots(evaluateBranches(template, resolvedData), resolvedData)
 }
 
 export async function renderTemplateFile(path: string, data: AsyncTemplateData, importMeta: ImportMeta) {
@@ -26,7 +28,33 @@ export async function renderTemplateFile(path: string, data: AsyncTemplateData, 
   return await renderTemplate(template, data)
 }
 
-function renderTemplateBasic(template: string, data: TemplateData) {
+function evaluateBranches(template: string, data: TemplateData) {
+  const branches = findBranches(template)
+  const lines = template.split(/\r\n?|\n/)
+  for(const [key, branch] of Object.entries(branches)) {
+    if(!data[key]) {
+      lines.splice(branch.start, branch.end - branch.start)
+    }
+  }
+  return lines.join("\n")
+}
+
+function findBranches(template: string) {
+  const result: Record<string, {start: number, end: number}> = {}
+  for(const [lineIndex, line] of template.split(/\r\n?|\n/).entries()) {
+    const starts = templateBranchStartRE.exec(line)
+    const ends = templateBranchEndRE.exec(line)
+    if(starts) {
+      result[starts![1]] = {start: lineIndex, end: Infinity}
+    }
+    if(ends) {
+      result[ends![1]].end = lineIndex
+    }
+  }
+  return result
+}
+
+function evaluateSlots(template: string, data: TemplateData) {
   return template.replaceAll(templateSlotRE, replaceTemplateSlot)
 
   function replaceTemplateSlot(_: string, slotName: string) {
