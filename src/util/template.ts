4c30801bbc372634479e61124fa878cb97e05aa5
Patrick Canfield <pscale01@gmail.com>
2023-02-23 16:04:29 -0800

    rethink templates again

--- src/util/template.ts
+++ src/util/template.ts
@@ -1,18 +1,14 @@
 import { LinkStruct, _getStyleSheets } from "~/internal/build_state.ts";
 import { readTextFileFromModule, resolvePath } from "~/util.ts";
+import { generateTemplateCode } from "~/util/template_codegen.ts";
 
-const slotRE = /\$\{(.*?)\}/g;
-const loopElementSlotRE = /\$\{itemOf: (.*?)\}/g;
-const multStartRE = /^\s*<!--\s*\*\:(.*?)\s*-->$/g;
-const multEndRE = /^\s*<!--\s*\/\:(.*?)\s*-->$/g;
-
-export type TemplateValueScalar = string | number;
-export type TemplateValue = TemplateValueScalar | Array<TemplateValueScalar>;
+export type TemplateValue = ReturnType<typeof JSON.parse>;
 export type AsyncTemplateData = Record<
   string,
   Promise<TemplateValue> | TemplateValue
 >;
 export type TemplateData = Record<string, TemplateValue>;
+export type TemplateDataCollection = Array<TemplateValue>;
 
 export const SPECIAL_SLOT_SYTLESHEETS = Symbol("StyleSheets");
 
@@ -28,121 +24,25 @@ export async function renderTemplate(
     resolvedData[key] = await data[key];
   }
   Object.assign(resolvedData, derivedData);
-  return evaluateSlots(evaluateBranches(template, resolvedData), resolvedData);
+  const jsString = generateTemplateCode(template, resolvedData);
+  console.log(jsString);
+  return eval(jsString);
 }
 
-let _currentTemplateFile: string | null = null
+let _currentTemplateFile: string | null = null;
 export async function renderTemplateFile(
   path: string,
   data: AsyncTemplateData,
-  importMeta: ImportMeta,
+  importMeta: ImportMeta
 ) {
   const template = await readTextFileFromModule(path, importMeta);
   // TODO
-  _currentTemplateFile = resolvePath(path, importMeta)
+  _currentTemplateFile = resolvePath(path, importMeta);
   const result = await renderTemplate(template, data);
-  _currentTemplateFile = null
-  return result
-}
-
-function countLoops(expressionValue: TemplateValue | Array<TemplateValue> ): number {
-  return expressionValue instanceof Array ? expressionValue.length : expressionValue ? 1 : 0
-}
-
-function evaluateBranches(template: string, data: TemplateData) {
-  const branches = findBranches(template),
-    linesIn = template.split(/\r\n?|\n/),
-    linesOut = [] as Array<string>
-  let inputIndex = 0
-
-  while(inputIndex < linesIn.length) {
-    const branch = branches.byLine[inputIndex]
-    if(branch?.isStart) {
-      const expressionValue = evaluateVariable(data, branch.expressionId)
-      const loopMax = countLoops(expressionValue)
-      const branchEnd = branches.byExpression[branch.expressionId].inst[branch.instIndex].end
-      const loopBody = linesIn.slice(inputIndex + 1, branchEnd)
-      if(branchEnd < Infinity) {
-        linesOut.push(linesIn[inputIndex])
-        for(let loopCount = 0; loopCount < loopMax; loopCount++) {
-          linesOut.push(...loopBody.map((line) => evaluateLoopSlots(line, data, loopCount)))
-        }
-        inputIndex = branchEnd
-      } else {
-        throw new Error(`Template contains unterminated branch. ${_currentTemplateFile} L${inputIndex+1}: ${linesIn[inputIndex].trim()}`)
-      }
-    }
-    linesOut.push(linesIn[inputIndex])
-    inputIndex++
-  }
-
-  return linesOut.join("\n")
-}
-
-function findBranches(template: string) {
-  const result = {
-    byLine: [] as Array<{expressionId: string, isStart: boolean, instIndex: number}>,
-    byExpression: {} as Record<string, {inst: Array<{start: number, end: number}>}>,
-  }
-  const stack = [] as Array<{start: number, expressionId: string, instIndex: number}>;
-  for (const [lineIndex, line] of template.split(/\r\n?|\n/).entries()) {
-    const starts = multStartRE.exec(line);
-    multStartRE.lastIndex = 0
-    const ends = multEndRE.exec(line);
-    multEndRE.lastIndex = 0
-    if (starts) {
-      const expressionId = starts[1]
-      const expressionInfo = result.byExpression[expressionId] ||= {inst: []}
-      const instIndex = expressionInfo.inst.length
-      result.byLine[lineIndex] = {expressionId, isStart: true, instIndex}
-      expressionInfo.inst.push({ start: lineIndex, end: Infinity});
-      stack.push({start: lineIndex, expressionId, instIndex});
-    }
-    if (ends) {
-      const expressionId = ends[1]
-      const pairInfo = stack.pop()
-      if(pairInfo && pairInfo.expressionId === expressionId) {
-        const expressionInfo = result.byExpression[expressionId] ||= {inst: []}
-        const instItem = expressionInfo.inst[pairInfo?.instIndex]
-        result.byLine[lineIndex] = {expressionId, isStart: false, instIndex: pairInfo.instIndex}
-        instItem.end = lineIndex
-      } else {
-        throw new Error(`Template contains unbalanced branch. ${_currentTemplateFile} L${lineIndex+1}: ${ends[0].trim()}`)
-      }
-    }
-  }
+  _currentTemplateFile = null;
   return result;
 }
 
-function evaluateSlots(template: string, data: TemplateData) {
-  return template.replaceAll(slotRE, replacer);
-
-  function replacer(_: string, slotName: string) {
-    return "" + evaluateVariable(data, slotName)
-  }
-}
-
-function evaluateLoopSlots(template: string, data: TemplateData, loopIndex: number) {
-  return template.replaceAll(loopElementSlotRE, replacer);
-
-  function replacer(_: string, slotName: string) {
-    return "" + evaluateArrayLookup(data, slotName, loopIndex)
-  }
-}
-
-function evaluateVariable(context: TemplateData, varName: string) {
-  if(!(varName in context)) {
-    console.warn(`WARN: Template "${_currentTemplateFile}" contains unbound variable "${varName}"`)
-  }
-  return context[varName];
-}
-
-function evaluateArrayLookup(context: TemplateData, varName: string, index: number) {
-  if(!(varName in context)) {
-    console.warn(`WARN: Template "${_currentTemplateFile}" contains unbound variable "${varName}"`)
-  }
-  return context[varName][index]
-}
 
 function renderLinks(links: Array<LinkStruct>) {
   return links
---
+++ src/util/template_codegen.ts
@@ -0,0 +1,159 @@
+import { TemplateData, TemplateDataCollection } from "~/util/template.ts";
+
+const loopStartRE = /^\s*<!--\s*#map (.+?) (.*?)\s*-->$/g;
+const loopEndRE = /^\s*<!--\s*\/map (.+?) (.*?)\s*-->$/g;
+
+/* BEGIN Templates */
+let $renderBody: void;
+let linesOut: Array<string>;
+let $context: void;
+function renderTemplateTemplate() {
+  $context;
+  $renderBody;
+  return linesOut.join("\n");
+}
+
+let $items: TemplateDataCollection;
+let $loopBody: void;
+function loopAppendLinesOut() {
+  const items = $items instanceof Array ? $items : $items ? [$items] : [];
+  for (const $item of items) {
+    $loopBody;
+  }
+}
+function appendLinesOut(line: string) {
+  linesOut.push(line);
+}
+/* END Templates */
+
+export function generateTemplateCode(template: string, data: TemplateData) {
+  const linesIn = template.split(/\r\n?|\n/);
+  const renderBodyLines = [
+    `const linesOut = []`,
+    appendLinesOut.toString(),
+    ...generateLines(linesIn, data),
+  ];
+
+  return renderJsIife(renderTemplateTemplate, {
+    $renderBody: renderBodyLines.join(";\n"),
+    $context: renderJsContext(data),
+  });
+}
+
+
+function generateLines(linesIn: Array<string>, data: TemplateData) {
+  const renderBodyLines = [];
+  const loops = findLoops(linesIn);
+  let lineIndex = 0;
+  while (lineIndex < linesIn.length) {
+    const line = linesIn[lineIndex];
+    const loop = loops.byLine[lineIndex];
+
+    renderBodyLines.push(`appendLinesOut(${renderJsString(line)})`);
+
+    if (loop) {
+      if (loop.endLineIndex < Infinity) {
+        const loopBodyLength = loop.endLineIndex - lineIndex - 1;
+        const loopBodyLines = linesIn.slice(lineIndex + 1, loop.endLineIndex);
+        const loopBodyLinesExpanded = generateLines(loopBodyLines, data);
+        const loopCode = renderJsIife(loopAppendLinesOut, {
+          ...loop.identifiers,
+          $loopBody: loopBodyLinesExpanded.join(";\n"),
+        });
+        renderBodyLines.push(loopCode);
+        lineIndex += loopBodyLength;
+      } else {
+        throw new Error(
+          `Template contains unterminated loop. L${
+            lineIndex + 1
+          }: ${line.trim()}`
+        );
+      }
+    }
+    lineIndex++;
+  }
+  return renderBodyLines;
+}
+
+function renderJs(jsTemplate: string, params: Record<string, string>) {
+  const jsTemplateParamRE = /\$[^\{].+?\b/g;
+  let result = jsTemplate;
+  result = jsTemplate.replaceAll(jsTemplateParamRE, (paramName: string) => {
+    if (params[paramName]) {
+      return params[paramName];
+    } else {
+      throw new Error(`No replacement for js param ${paramName}`);
+    }
+  });
+  // result = jsTemplate.replaceAll(jsTemplateParamRE, (paramName: string) => {
+  //   if (params[paramName]) {
+  //     return params[paramName];
+  //   } else {
+  //     throw new Error(`No replacement for js param ${paramName}`);
+  //   }
+  // });
+  return result;
+}
+
+function renderJsString(str: string) {
+  return `\`${str.replaceAll(/`/g, "\\`").trim()}\``;
+}
+
+function renderJsIife(fn: () => void, params: Record<string, string>) {
+  return `(${renderJs(fn.toString(), params)})()`;
+}
+function renderJsContext(context: TemplateData) {
+  const linesOut: Array<string> = [];
+  for (const [name, value] of Object.entries(context)) {
+    linesOut.push(`const ${name} = ${JSON.stringify(value)}`);
+  }
+  return linesOut.join(";\n");
+}
+
+
+interface LoopInfo {
+  identifiers: { $items: string; $item: string };
+  endLineIndex: number;
+}
+function findLoops(templateLines: Array<string>) {
+  const result = {
+    byLine: [] as Array<LoopInfo>,
+  };
+  const stack = [] as Array<{ start: number; expressionId: string }>;
+  const expressionMap = {} as Record<string, { start: number }>;
+  for (const [lineIndex, line] of templateLines.entries()) {
+    const starts = loopStartRE.exec(line);
+    loopStartRE.lastIndex = 0;
+    const ends = loopEndRE.exec(line);
+    loopEndRE.lastIndex = 0;
+    const [_, $items, $item] = starts || ends || [];
+    const expressionId = `${$items}:${$item}`;
+    if (starts) {
+      result.byLine[lineIndex] = {
+        identifiers: {
+          $items,
+          // defaults to "item" if not specified
+          $item: $item ? $item : "item"
+        },
+        endLineIndex: Infinity,
+      };
+      expressionMap[expressionId] = { start: lineIndex };
+      stack.push({ start: lineIndex, expressionId });
+    }
+    if (ends) {
+      const pairInfo = stack.pop();
+      if (pairInfo && pairInfo.expressionId === expressionId) {
+        const start = expressionMap[expressionId].start;
+        result.byLine[start].endLineIndex = lineIndex;
+      } else {
+        throw new Error(
+          `Template contains unbalanced branch. L${
+            lineIndex + 1
+          }: ${line.trim()}`
+        );
+      }
+    }
+  }
+
+  return result;
+}
