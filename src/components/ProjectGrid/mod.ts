3ed40d20c0ba5fae8defab4c15757fb65dcf9c92
Patrick Canfield <pscale01@gmail.com>
2023-02-24 14:59:48 -0800

    simplify

--- src/components/ProjectGrid/mod.ts
+++ src/components/ProjectGrid/mod.ts
@@ -8,20 +8,8 @@ export interface ProjectGridProps {
 
 useStyleSheet('./style.css', import.meta)
 
-export function Item(props: Project) {
-  return Object.assign(props, {
-    get img() {
-      return props.previewType === "img"
-    },
-    get video() {
-      return props.previewType === "video"
-    }
-  })
-}
-
 export function ProjectGrid (props: ProjectGridProps) {
   return renderTemplateFile("./grid.html", {
-    // TODO make a helper for this pattern or support collections in templates
-    items: props.items.map(Item)
+    items: props.items
   }, import.meta);
 }
