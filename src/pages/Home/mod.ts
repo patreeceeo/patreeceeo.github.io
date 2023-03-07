9980ff93999bfc3fb3244bdc11f69acc0431b3eb
Patrick Canfield <pscale01@gmail.com>
2023-03-07 09:23:30 -0800

    add Liam's reference

--- src/pages/Home/mod.ts
+++ src/pages/Home/mod.ts
@@ -6,9 +6,21 @@ import * as Posts from "~/posts/mod.ts";
 
 const kindWords: Array<KindWordsProps> = [
   {
+    photo: 'images/liam.norris.jpg',
+    name: 'Liam Norris',
+    distinction: 'Senior Software Engineer',
+    quote: `
+      <p>
+      Patrick is a master across a wide range of full-stack frameworks. His focus and attention to detail while we worked together at Synack, Inc were incredibly noteworthy. As early employees there, we built much of the company’s nascent functionality that the founders have since used to raise >$100M of venture capital funding with. The depth of Patrick’s front end (UI/UX) expertise is driven not only by the fearlessness that he dives into unfamiliar technologies with but also his talent as an illustrator. Pay attention to what Patrick has to say: he is a calming voice of sharp intellect.
+    </p>
+        `,
+    href: "http://www.thoughtcodex.com/",
+
+  },
+  {
     photo: "images/Aidan.webp",
     name: "Aidan Caruso",
-    distinction: "Engineer & Technical Artist, Mappa Labs, 2023",
+    distinction: "Engineer & Technical Artist",
     quote: `
         <p>
         Patrick got me on my feet with some new technologies: Code reviews from him were informative, being detail-oriented but without missing the bigger picture, which really optimized what I had wrote. In-line with code reviews he helped set up an efficient Github workflow that streamlined our development process. All around, he was a substantial help to both myself and our project.
@@ -19,7 +31,7 @@ const kindWords: Array<KindWordsProps> = [
   {
     photo: "images/MikeDavis.jpeg",
     name: "Mike Davis",
-    distinction: "Senior Engineer, Constant Contact, 2014",
+    distinction: "Technical Product Leader & Entrepreneur",
     quote: `
         <p>
         Patrick is both an incredibly skilled and intelligent engineer (not to mention person in general). He strives for high code quality and ensures the bar is held high for his teammates too. His experience and insight makes him a great addition to any team. I would happily work with Patrick again.
