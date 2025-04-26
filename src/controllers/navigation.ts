00f98e1cbd1df093a7cae60b0c9ed2fa06e8caba
Patrick Canfield <578371+patreeceeo@users.noreply.github.com>
2025-04-26 15:44:38 -0700

    test sthing

--- src/controllers/navigation.ts
+++ src/controllers/navigation.ts
@@ -3,7 +3,7 @@ import { Controller } from '@hotwired/stimulus';
 export class NavSelectController extends Controller<HTMLSelectElement> {
   connect() {
     const { element } = this;
-    element.value = window.location.pathname;
+    // element.value = window.location.pathname;
   }
 
   navigateToSelection() {
