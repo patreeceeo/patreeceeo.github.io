b8d415bebd9d91fe4e4fb117fa5ae8bbddf33249
Patrick Canfield <578371+patreeceeo@users.noreply.github.com>
2025-04-26 17:36:58 -0700

    really really

--- src/controllers/navigation.ts
+++ src/controllers/navigation.ts
@@ -5,18 +5,18 @@ export class NavSelectController extends Controller<HTMLSelectElement> {
     value: String,
   };
   connect() {
+    this.setValue();
+    // WebKit tries to preserve the select value across page reloads
+    // The only known workaround
+    setTimeout(this.setValue, 200);
+  }
+
+  setValue = () => {
     const { element } = this;
     element.value = this.data.get('value') as string;
   }
 
   navigateToSelection() {
     location.pathname = `/portfolio/${this.element.value}`;
-  }
-
-  /**
-  * Doing this before navigation prevents WebKit from trying to preserve the select value across page reloads
-  */
-  clearSelection() {
-    this.element.value = "";
   }
 }
